2f88821604d27c4b71f9f8c66c2152fb1cf1b7f4
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-11-09 16:10:52 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screen: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
     extend: {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
