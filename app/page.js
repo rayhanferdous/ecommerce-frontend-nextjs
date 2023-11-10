a17bf479e48efb4887865e39ddbdc8f07e2adb99
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-11-10 21:03:47 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import Hero from "@/components/index/Hero";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      index
+    <main>
+      <Hero />
     </main>
   );
 }
