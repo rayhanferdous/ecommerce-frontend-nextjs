1a7b33ca42ac20735c54ad0ab026963a70696505
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-11-10 21:03:35 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/layout/Footer";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
       <body>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
