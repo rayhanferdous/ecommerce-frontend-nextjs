ed1ac4ef9e8bfa41f75f728003a8b93f96e1569f
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-11-11 15:33:50 +0600

    Update Categories.jsx

--- components/index/Categories.jsx
+++ components/index/Categories.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { categories } from "./constants";
+import CategoriyCard from "../utils/CategoriyCard";
 
 const Categories = () => {
   return (
@@ -7,84 +9,14 @@ const Categories = () => {
         shop by category
       </h2>
       <div className="grid grid-cols-3 gap-3">
-        <div className="relative rounded-sm overflow-hidden group">
-          <img
-            src="assets/images/category/category-1.jpg"
-            alt="category 1"
-            className="w-full"
+        {categories.map((category, key) => (
+          <CategoriyCard
+            key={key}
+            image={category.image}
+            title={category.title}
+            link={category.link}
           />
-          <a
-            href="#"
-            className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition"
-          >
-            Bedroom
-          </a>
-        </div>
-        <div className="relative rounded-sm overflow-hidden group">
-          <img
-            src="assets/images/category/category-2.jpg"
-            alt="category 1"
-            className="w-full"
-          />
-          <a
-            href="#"
-            className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition"
-          >
-            Mattrass
-          </a>
-        </div>
-        <div className="relative rounded-sm overflow-hidden group">
-          <img
-            src="assets/images/category/category-3.jpg"
-            alt="category 1"
-            className="w-full"
-          />
-          <a
-            href="#"
-            className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition"
-          >
-            Outdoor
-          </a>
-        </div>
-        <div className="relative rounded-sm overflow-hidden group">
-          <img
-            src="assets/images/category/category-4.jpg"
-            alt="category 1"
-            className="w-full"
-          />
-          <a
-            href="#"
-            className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition"
-          >
-            Sofa
-          </a>
-        </div>
-        <div className="relative rounded-sm overflow-hidden group">
-          <img
-            src="assets/images/category/category-5.jpg"
-            alt="category 1"
-            className="w-full"
-          />
-          <a
-            href="#"
-            className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition"
-          >
-            Living Room
-          </a>
-        </div>
-        <div className="relative rounded-sm overflow-hidden group">
-          <img
-            src="assets/images/category/category-6.jpg"
-            alt="category 1"
-            className="w-full"
-          />
-          <a
-            href="#"
-            className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition"
-          >
-            Kitchen
-          </a>
-        </div>
+        ))}
       </div>
     </div>
   );
