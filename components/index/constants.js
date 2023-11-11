751dc16dd04d5592be233f91cd95d8ce479c22cb
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-11-11 15:33:57 +0600

    Create constants.js

---
+++ components/index/constants.js
@@ -0,0 +1,50 @@
+export const features = [
+  {
+    title: "Free Shipping",
+    icon: "/assets/images/icons/delivery-van.svg",
+    subtitle: "Order over $200",
+  },
+  {
+    title: "Money Rturns",
+    icon: "/assets/images/icons/money-back.svg",
+    subtitle: "30 days money returs",
+  },
+  {
+    title: "24/7 Support",
+    icon: "assets/images/icons/service-hours.svg",
+    subtitle: "Customer support",
+  },
+];
+
+export const categories = [
+  {
+    title: "Bedroom",
+    image: "/assets/images/category/category-1.jpg",
+    link: "/#",
+  },
+  {
+    title: "Mattrass",
+    image: "/assets/images/category/category-2.jpg",
+    link: "/#",
+  },
+  {
+    title: "Outdoor",
+    image: "/assets/images/category/category-3.jpg",
+    link: "/#",
+  },
+  {
+    title: "Sofa",
+    image: "/assets/images/category/category-4.jpg",
+    link: "/#",
+  },
+  {
+    title: "Living Room",
+    image: "/assets/images/category/category-5.jpg",
+    link: "/#",
+  },
+  {
+    title: "Kitchen",
+    image: "/assets/images/category/category-6.jpg",
+    link: "/#",
+  },
+];
