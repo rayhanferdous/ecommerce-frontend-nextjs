49073bdff8c9d1df7990cb7d82a3ceeeeedf683c
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-11-29 22:45:52 +0600

    Create footerConst.jsx

---
+++ components/utils/footer/footerConst.jsx
@@ -0,0 +1,86 @@
+export const footerLinks = [
+  {
+    title: "Solutions",
+    links: [
+      {
+        name: "Marketing",
+        path: "/#",
+      },
+      {
+        name: "Analitycs",
+        path: "/#",
+      },
+      {
+        name: "Commerce",
+        path: "/#",
+      },
+      {
+        name: "Insights",
+        path: "/#",
+      },
+    ],
+  },
+  {
+    title: "Support",
+    links: [
+      {
+        name: "Pricing",
+        path: "/#",
+      },
+      {
+        name: "Documentation",
+        path: "/#",
+      },
+      {
+        name: "Guides",
+        path: "/#",
+      },
+      {
+        name: "API Status",
+        path: "/#",
+      },
+    ],
+  },
+  {
+    title: "Solutions",
+    links: [
+      {
+        name: "Marketing",
+        path: "/#",
+      },
+      {
+        name: "Analitycs",
+        path: "/#",
+      },
+      {
+        name: "Commerce",
+        path: "/#",
+      },
+      {
+        name: "Insights",
+        path: "/#",
+      },
+    ],
+  },
+  {
+    title: "Support",
+    links: [
+      {
+        name: "Pricing",
+        path: "/#",
+      },
+      {
+        name: "Documentation",
+        path: "/#",
+      },
+      {
+        name: "Guides",
+        path: "/#",
+      },
+      {
+        name: "API Status",
+        path: "/#",
+      },
+    ],
+  },
+];
