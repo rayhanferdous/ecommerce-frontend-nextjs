b2b44cebff0e22e0fd0d772ec7934e2253d3e7f6
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-11-30 22:57:24 +0600

    Create headerConst.js

---
+++ components/utils/header/headerConst.js
@@ -0,0 +1,76 @@
+export const navLinks = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "shop",
+    path: "/shop",
+  },
+
+  {
+    name: "About Us",
+    path: "/about",
+  },
+  {
+    name: "Contact Us",
+    path: "/contact",
+  },
+];
+
+export const iconCardItems = [
+  {
+    path: "/#",
+    img: "heart.svg",
+    items: 8,
+    isIconNum: true,
+    name: "Wishlist",
+  },
+  {
+    path: "/#",
+    img: "shopping-bag.svg",
+    items: 2,
+    isIconNum: true,
+    name: "Cart",
+  },
+  {
+    path: "/#",
+    img: "user.svg",
+    items: 0,
+    isIconNum: false,
+    name: "Account",
+  },
+];
+
+export const allCategories = [
+  {
+    path: "/shop",
+    name: "Sofa",
+    img: "sofa.svg",
+  },
+  {
+    path: "/shop",
+    name: "Terarce",
+    img: "bed-2.svg",
+  },
+  {
+    path: "/shop",
+    name: "Bed",
+    img: "bed.svg",
+  },
+  {
+    path: "/shop",
+    name: "Office",
+    img: "office.svg",
+  },
+  {
+    path: "/shop",
+    name: "Outdoor",
+    img: "outdoor-cafe.svg",
+  },
+  {
+    path: "/shop",
+    name: "Mattress",
+    img: "bed-2.svg",
+  },
+];
