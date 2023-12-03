0030bb6a7719c3039c031cd422e974796e1fd83e
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-12-03 22:17:34 +0600

    Create SearchField.jsx

---
+++ components/utils/header/SearchField.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const SearchField = () => {
+  return (
+    <form className="w-full max-w-xl relative flex">
+      <span className="absolute left-4 top-3 text-lg text-gray-400">
+        <img
+          src="/assets/images/icons/search.svg"
+          alt="search"
+          className="w-6"
+        />
+      </span>
+      <input
+        type="text"
+        name="search"
+        id="search"
+        className="w-full border border-primary border-r-0 pl-12 py-3 pr-3 rounded-l-md focus:outline-none hidden md:flex"
+        placeholder="search"
+      />
+      <button className="text-center align-middle bg-primary border border-primary text-white px-8 rounded-r-md hover:bg-transparent hover:text-primary transition hidden md:flex md:items-center">
+        Search
+      </button>
+    </form>
+  );
+};
+
+export default SearchField;
