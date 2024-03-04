33993829151c61a3029846ddf802c074e5c8c354
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2024-03-04 23:11:50 +0600

    show recommended product on recommended section

--- components/index/Recommended.jsx
+++ components/index/Recommended.jsx
@@ -1,22 +1,23 @@
-import React from "react";
-import { recomandedForYou } from "./constants";
+import { fetchWooApi } from "@/app/lib/ApiHelper/fetchWooApi";
 import NewArrivalCard from "../utils/NewArrivalCard";
 
-const Recomanded = () => {
+const Recommended = async () => {
+  const products = await fetchWooApi("products");
+  const data = await products.json();
   return (
     <div className="container pb-16">
       <h2 className="text-2xl font-medium text-gray-800 uppercase mb-6">
         recomended for you
       </h2>
       <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-6">
-        {recomandedForYou.map((item, key) => (
+        {data.map((item, key) => (
           <NewArrivalCard
             key={key}
             name={item.name}
-            productImage={item.productImage}
-            price={item.price}
-            actualPrice={item.actualPrice}
-            review={item.review}
+            productImage={item.images[0].src}
+            price={item.regular_price}
+            actualPrice={item.price}
+            review={item.rating_count}
           />
         ))}
       </div>
@@ -24,4 +25,4 @@ const Recomanded = () => {
   );
 };
 
-export default Recomanded;
+export default Recommended;
