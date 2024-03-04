d0aa597d86bcd05372915d9f4be9a9cef92d40f3
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2024-03-04 23:10:09 +0600

    show new arrival products on new arrival sections

--- components/index/NewArrival.jsx
+++ components/index/NewArrival.jsx
@@ -1,22 +1,24 @@
-import React from "react";
-import { newArrivals } from "./constants";
+import { fetchWooApi } from "@/app/lib/ApiHelper/fetchWooApi";
 import NewArrivalCard from "../utils/NewArrivalCard";
 
-const NewArrival = () => {
+const NewArrival = async () => {
+  const products = await fetchWooApi("products");
+  const data = await products.json();
+
   return (
     <div className="container pb-16">
       <h2 className="text-2xl font-medium text-gray-800 uppercase mb-6">
         top new arrival
       </h2>
       <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-6">
-        {newArrivals.map((item, key) => (
+        {data.map((item, key) => (
           <NewArrivalCard
             key={key}
             name={item.name}
-            productImage={item.productImage}
-            price={item.price}
-            actualPrice={item.actualPrice}
-            review={item.review}
+            productImage={item.images[0].src}
+            price={item.regular_price}
+            actualPrice={item.price}
+            review={item.rating_count}
           />
         ))}
       </div>
